79fdccb68144018d42060053bfb84ff77b1795dc
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-01-10 23:29:33 +0530

    BitManipulation

---
+++ BitManipulation/ReplaceBits.cpp
@@ -0,0 +1,40 @@
+/*
+Given a 32 bit number, N and M ,and two bit positions i and j.
+Write a method to set all bits between i and j in N equal to M
+M (becomes a substring of N locationed at and starting at j)
+
+Example
+N=10000000000
+M=10101
+i=2, j=6
+
+Output:
+1001010100
+*/
+
+/// Replace bits in N by M
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void clearBitsInRange(int &n, int i, int j){
+    int a=(~0)<<(j+1);
+    int b=(1<<i)-1;
+    int mask=a|b;
+    n=n&mask;
+}
+
+void replaceBits(int &n, int i, int j, int m){
+    clearBitsInRange(n,i,j);
+    int mask=(m<<i);
+    n=n|mask;
+}
+
+int main(){
+    int n=15;
+    int i=1, j=3;
+    int m=2;
+    replaceBits(n,i,j,m);
+    cout<<n<<endl;
+    return 0;
+}
