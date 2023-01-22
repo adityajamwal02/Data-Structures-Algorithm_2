25c11017e6e463071271bc8a6eb93830732fac78
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-01-22 14:35:36 +0530

    BitManipulation

--- BitManipulation/CountingSetBits.cpp
+++ BitManipulation/CountingSetBits.cpp
@@ -15,6 +15,19 @@ int count_bits(int n){
 return count;
 }
 
+/*
+Counting Set Bits Hack
+
+int count_bits_hack(int n){
+    int ans=0;
+    while(n>0){
+        n=n&(n-1);
+        ans++;
+    }
+    return ans;
+}
+*/
+
 
 int main(){
     int n;
