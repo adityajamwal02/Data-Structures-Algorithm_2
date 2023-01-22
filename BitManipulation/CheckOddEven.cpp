34dafe1855d6116d0779601b66e12e44ba3859a3
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-01-22 13:40:34 +0530

    BitManipulation

---
+++ BitManipulation/CheckOddEven.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    int x;
+    cin>>x;
+    if(x&1){
+        cout<<"ODD"<<endl;
+    }else{
+        cout<<"EVEN"<<endl;
+    }
+
+    return 0;
+}
